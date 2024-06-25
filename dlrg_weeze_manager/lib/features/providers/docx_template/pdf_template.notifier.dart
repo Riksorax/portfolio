aa1cdd79060eb60bc3782cb06936c11705a4a9b4
Riksorax <Franksp20.fs@gmail.com>
2024-06-25 10:51:12 +0200

    First try to edited the member card

--- dlrg_weeze_manager/lib/features/providers/docx_template/pdf_template.notifier.dart
+++ dlrg_weeze_manager/lib/features/providers/docx_template/pdf_template.notifier.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
+import 'dart:ui';
 
-import 'package:docx_template/docx_template.dart';
+import 'package:excel/excel.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
 
 import '../file_picker/file_picker.provider.dart';
 
@@ -22,7 +24,7 @@ class PdfTemplate extends _$PdfTemplate {
     if (!pathExists) {
       return null;
     }
-    var completePath = '${path.path}\\MitgliedsausweisTemplate.docx';
+    var completePath = '${path.path}\\MitgliedsausweisTemplate.pdf';
     var file = await File(completePath).exists();
     if (!file) {
       return null;
@@ -34,10 +36,14 @@ class PdfTemplate extends _$PdfTemplate {
   Future<void> fillPlaceholderPDF() async {
     // Laden Sie die PDF-Datei
     var docxTemplate = await loadPdfTemplate();
-    var docx = File(docxTemplate!);
-    final template = await DocxTemplate.fromBytes( await docx.readAsBytes());
+    var pdfPath = File(docxTemplate!).readAsBytesSync();
+    final PdfDocument document =
+    PdfDocument(inputBytes: pdfPath);
 
-    template.substitute
+    PdfTextExtractor extractor = PdfTextExtractor(document);
+    var result = extractor.extractText();
+    String modifiedText = result;
+    final PdfPage page = document.pages[0];
 
     // Platzhalter und deren Werte definieren
     final placeholders = {
@@ -46,12 +52,56 @@ class PdfTemplate extends _$PdfTemplate {
       '{{NUMBER}}': '848798798',
     };
 
+    placeholders.forEach((placeholder, value) {
+      modifiedText = modifiedText.replaceAll(placeholder, value);
+    });
 
+    if (result != modifiedText) {
+      // Originaltext löschen, indem ein weißes Rechteck über den Text gezeichnet wird
+      final PdfGraphics graphics = page.graphics;
+      final Rect rect = Rect.fromLTWH(0, 0, page.getClientSize().width - 110, page.getClientSize().height);
+      graphics.drawRectangle(
+        brush: PdfSolidBrush(PdfColor(255, 255, 255)),
+        bounds: rect,
+      );
+
+      for (var value in extractor.extractTextLines()) {
+        var fontSize = value.fontSize;
+        var bounds = value.bounds;
+        var fontName = value.fontName;
+        var text = value.text;
+
+        // Modifizierten Text hinzufügen
+        graphics.drawString(
+          text,
+          PdfStandardFont(getFontFamily(fontName), fontSize),
+          bounds: bounds,
+          brush: PdfSolidBrush(PdfColor(204, 204, 204))
+        );
+      }
+
+    }
 
     // Speichern Sie die Datei auf dem Gerät
     const String outputPath = 'C:\\Users\\Frank\\Documents\\DLRG\\AusweisTemplate\\MitgliedsausweisTemplateCopy.pdf';
-    final File outputFile = File(outputPath);
-    //await outputFile.writeAsBytes(updatedPdfBytes);
+
+    // Neue PDF-Datei speichern
+    final List<int> newBytes = document.saveSync();
+    final File newFile = File(outputPath);
+    await newFile.writeAsBytes(newBytes);
+
+    // PDF-Dokument schließen
+    document.dispose();
+  }
+
+  PdfFontFamily getFontFamily(String fontName) {
+    for (var family in PdfFontFamily.values) {
+      if (family.toString().split('.').last.toLowerCase() == fontName.toLowerCase()) {
+        return family;
+      }
+    }
+    // Fallback zu einer Standardschriftart, falls nicht gefunden
+    return PdfFontFamily.helvetica;
   }
 
 }

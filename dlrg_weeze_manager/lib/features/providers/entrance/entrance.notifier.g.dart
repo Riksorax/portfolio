5dfcd3469a3c6f402a25ff74afbb1cf971de2b20
Riksorax <Franksp20.fs@gmail.com>
2025-02-01 23:49:56 +0100

    update notifier

---
+++ dlrg_weeze_manager/lib/features/providers/entrance/entrance.notifier.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'entrance.notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$entranceNotifierHash() => r'896da6dca9361f0bef686e94f51689ee4966caf6';
+
+/// See also [EntranceNotifier].
+@ProviderFor(EntranceNotifier)
+final entranceNotifierProvider =
+    AutoDisposeNotifierProvider<EntranceNotifier, List<Member>>.internal(
+  EntranceNotifier.new,
+  name: r'entranceNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$entranceNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$EntranceNotifier = AutoDisposeNotifier<List<Member>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ dlrg_weeze_manager/lib/features/providers/nfc/nfc_read.notifier.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'nfc_read.notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$nfcReadNotifierHash() => r'66c6e3922f1946098875078887ccea7f67263932';
+
+/// See also [NfcReadNotifier].
+@ProviderFor(NfcReadNotifier)
+final nfcReadNotifierProvider =
+    AutoDisposeNotifierProvider<NfcReadNotifier, bool>.internal(
+  NfcReadNotifier.new,
+  name: r'nfcReadNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$nfcReadNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$NfcReadNotifier = AutoDisposeNotifier<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
